12a11b9d3227af937e4805df5cc82ab02b49529f
abdelouahed akharaze <112736714+abdelouahedakharaze@users.noreply.github.com>
2024-10-12 19:10:13 +0100

    Modify BotaniKart_Backend/users/serializers.py

--- BotaniKart_Backend/users/serializers.py
+++ BotaniKart_Backend/users/serializers.py
@@ -1,39 +1,15 @@
 from rest_framework import serializers
-from django.contrib.auth import get_user_model
-from django.contrib.auth.password_validation import validate_password
+from .models import CustomUser, Address
 
-User = get_user_model()
+class AddressSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Address
+        fields = ['id', 'title', 'address_line_1', 'address_line_2', 'country', 'city', 'postal_code', 'landmark', 'phone_number']
 
 class UserSerializer(serializers.ModelSerializer):
-    password = serializers.CharField(write_only=True, required=True, validators=[validate_password])
-    password2 = serializers.CharField(write_only=True, required=True)
+    addresses = AddressSerializer(many=True, read_only=True)
 
     class Meta:
-        model = User
-        fields = ('username', 'password', 'password2', 'email', 'first_name', 'last_name', 'bio', 'is_seller')
-        extra_kwargs = {
-            'first_name': {'required': True},
-            'last_name': {'required': True}
-        }
-
-    def validate(self, attrs):
-        if attrs['password'] != attrs['password2']:
-            raise serializers.ValidationError({"password": "Password fields didn't match."})
-        return attrs
-
-    def create(self, validated_data):
-        user = User.objects.create(
-            username=validated_data['username'],
-            email=validated_data['email'],
-            first_name=validated_data['first_name'],
-            last_name=validated_data['last_name'],
-            bio=validated_data.get('bio', ''),
-            is_seller=validated_data.get('is_seller', False)
-        )
-        user.set_password(validated_data['password'])
-        user.save()
-        return user
-
-class UserLoginSerializer(serializers.Serializer):
-    username = serializers.CharField(required=True)
-    password = serializers.CharField(required=True)
+        model = CustomUser
+        fields = ['id', 'username', 'email', 'first_name', 'last_name', 'bio', 'is_vendor', 'store_name', 'store_description', 'avatar', 'birth_of_date', 'phone_number', 'addresses']
+        read_only_fields = ['id']
