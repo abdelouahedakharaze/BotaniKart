562b5596d9fd7430bdde0813cf9110126e9aaf53
abdelouahed akharaze <112736714+abdelouahedakharaze@users.noreply.github.com>
2024-10-12 17:03:54 +0100

    Modify BotaniKart_Backend/BotaniKart_Backend/urls.py

--- BotaniKart_Backend/BotaniKart_Backend/urls.py
+++ BotaniKart_Backend/BotaniKart_Backend/urls.py
@@ -1,23 +1,25 @@
-"""
-URL configuration for BotaniKart_Backend project.
-
-The `urlpatterns` list routes URLs to views. For more information please see:
-    https://docs.djangoproject.com/en/5.1/topics/http/urls/
-Examples:
-Function views
-    1. Add an import:  from my_app import views
-    2. Add a URL to urlpatterns:  path('', views.home, name='home')
-Class-based views
-    1. Add an import:  from other_app.views import Home
-    2. Add a URL to urlpatterns:  path('', Home.as_view(), name='home')
-Including another URLconf
-    1. Import the include() function: from django.urls import include, path
-    2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
-"""
 from django.contrib import admin
 from django.urls import path, include
-from django.conf import settings  # Import settings to use DEBUG and MEDIA_URL
-from django.conf.urls.static import static  # Import static for serving media files
+from django.conf import settings
+from django.conf.urls.static import static
+from rest_framework import permissions
+from drf_yasg.views import get_schema_view
+from drf_yasg import openapi
+from utils.translation import LanguageView
+
+# Schema view for Swagger
+schema_view = get_schema_view(
+   openapi.Info(
+      title="Your API Title",
+      default_version='v1',
+      description="API documentation for your project",
+      terms_of_service="https://www.google.com/policies/terms/",
+      contact=openapi.Contact(email="contact@yourapi.local"),
+      license=openapi.License(name="BSD License"),
+   ),
+   public=True,
+   permission_classes=(permissions.AllowAny,),
+)
 
 urlpatterns = [
     path('admin/', admin.site.urls),
@@ -25,6 +27,19 @@ urlpatterns = [
     path('api/products/', include('products.urls')),
     path('api/blog/', include('blog.urls')),
     path('api/cart/', include('cart.urls')),
+    path('api/orders/', include('orders.urls')),
+    path('api/reviews/', include('reviews.urls')),
+    path('api/newsletter/', include('newsletter.urls')),
+    path('api/recommendations/', include('recommendations.urls')),
+    path('api/analytics/', include('analytics.urls')),
+    path('api/loyalty/', include('loyalty.urls')),
+    path('api/social/', include('social.urls')),
+    path('api/language/', LanguageView.as_view(), name='language-test'),
+
+    # Swagger UI
+    path('swagger/', schema_view.with_ui('swagger', cache_timeout=0), name='schema-swagger-ui'),
+    # Redoc UI
+    path('redoc/', schema_view.with_ui('redoc', cache_timeout=0), name='schema-redoc'),
 ]
 
 if settings.DEBUG:
