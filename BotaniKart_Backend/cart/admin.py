2ace209e9fcfbba5ff18b5b37b9a28d8cc62038f
abdelouahed akharaze <112736714+abdelouahedakharaze@users.noreply.github.com>
2024-10-12 17:40:55 +0100

    Modify BotaniKart_Backend/cart/admin.py

--- BotaniKart_Backend/cart/admin.py
+++ BotaniKart_Backend/cart/admin.py
@@ -1,8 +1,3 @@
 from django.contrib import admin
-from .models import CartItem
 
-@admin.register(CartItem)
-class CartItemAdmin(admin.ModelAdmin):
-    list_display = ('user', 'product', 'quantity', 'added_at')
-    list_filter = ('user', 'product', 'added_at')
-    search_fields = ('user__username', 'product__name')
+# Register your models here.
