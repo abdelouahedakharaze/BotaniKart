20904972fc276d11e47c847f3d6a43f647b46523
abdelouahed akharaze <112736714+abdelouahedakharaze@users.noreply.github.com>
2024-10-12 18:48:05 +0100

    Modify BotaniKart_Backend/users/admin.py

--- BotaniKart_Backend/users/admin.py
+++ BotaniKart_Backend/users/admin.py
@@ -1,11 +1,3 @@
 from django.contrib import admin
-from django.contrib.auth.admin import UserAdmin
-from .models import CustomUser
 
-class CustomUserAdmin(UserAdmin):
-    model = CustomUser
-    list_display = ['email', 'username', 'first_name', 'last_name', 'is_staff']
-    fieldsets = UserAdmin.fieldsets + ((None, {'fields': ('bio',)}),)
-    add_fieldsets = UserAdmin.add_fieldsets + ((None, {'fields': ('bio',)}),)
-
-admin.site.register(CustomUser, CustomUserAdmin)
+# Register your models here.
