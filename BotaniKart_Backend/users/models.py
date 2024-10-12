52b2927e19c47088405c77bcc49846a3801f5ea9
abdelouahed akharaze <112736714+abdelouahedakharaze@users.noreply.github.com>
2024-10-12 19:06:11 +0100

    Modify BotaniKart_Backend/users/models.py

--- BotaniKart_Backend/users/models.py
+++ BotaniKart_Backend/users/models.py
@@ -7,6 +7,30 @@ class CustomUser(AbstractUser):
     is_vendor = models.BooleanField(default=False)
     store_name = models.CharField(max_length=100, blank=True)
     store_description = models.TextField(blank=True)
+    paypal_email = models.EmailField(blank=True)
+    paypal_client_id = models.CharField(max_length=255, blank=True)
+    paypal_client_secret = models.CharField(max_length=255, blank=True)
+    stripe_account_id = models.CharField(max_length=255, blank=True)
+    avatar = models.ImageField(upload_to='avatars/', null=True, blank=True)
+    birth_of_date = models.DateField(null=True, blank=True)
+    phone_number = models.CharField(max_length=20, blank=True)
+    deleted_at = models.DateTimeField(null=True, blank=True)
     
     def __str__(self):
         return self.username
+
+class Address(models.Model):
+    user = models.ForeignKey(CustomUser, on_delete=models.CASCADE, related_name='addresses')
+    title = models.CharField(max_length=100)
+    address_line_1 = models.CharField(max_length=255)
+    address_line_2 = models.CharField(max_length=255, blank=True)
+    country = models.CharField(max_length=100)
+    city = models.CharField(max_length=100)
+    postal_code = models.CharField(max_length=20)
+    landmark = models.CharField(max_length=255, blank=True)
+    phone_number = models.CharField(max_length=20)
+    created_at = models.DateTimeField(auto_now_add=True)
+    deleted_at = models.DateTimeField(null=True, blank=True)
+
+    def __str__(self):
+        return f"{self.title} - {self.user.username}"
