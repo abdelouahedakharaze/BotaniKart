a605fca9c5d789dfd5c8c92be453d983ac2e35a7
abdelouahed akharaze <112736714+abdelouahedakharaze@users.noreply.github.com>
2024-10-12 17:12:54 +0100

    Modify BotaniKart_Backend/blog/admin.py

--- BotaniKart_Backend/blog/admin.py
+++ BotaniKart_Backend/blog/admin.py
@@ -1,20 +1,3 @@
 from django.contrib import admin
-from .models import Article, Comment, Like
 
-@admin.register(Article)
-class ArticleAdmin(admin.ModelAdmin):
-    list_display = ('title', 'author', 'created_at', 'updated_at')
-    list_filter = ('author', 'created_at')
-    search_fields = ('title', 'content', 'author__username')
-
-@admin.register(Comment)
-class CommentAdmin(admin.ModelAdmin):
-    list_display = ('article', 'author', 'created_at')
-    list_filter = ('article', 'author', 'created_at')
-    search_fields = ('content', 'author__username', 'article__title')
-
-@admin.register(Like)
-class LikeAdmin(admin.ModelAdmin):
-    list_display = ('article', 'user', 'created_at')
-    list_filter = ('article', 'user', 'created_at')
-    search_fields = ('user__username', 'article__title')
+# Register your models here.
