2447008a2202d4d9025ee55266e65af231c6dfb8
abdelouahed akharaze <112736714+abdelouahedakharaze@users.noreply.github.com>
2024-10-12 09:33:42 +0100

    vendor added to user model

--- BotaniKart_Backend/users/models.py
+++ BotaniKart_Backend/users/models.py
@@ -4,6 +4,9 @@ from django.db import models
 class CustomUser(AbstractUser):
     email = models.EmailField(unique=True)
     bio = models.TextField(blank=True)
+    is_vendor = models.BooleanField(default=False)
+    store_name = models.CharField(max_length=100, blank=True)
+    store_description = models.TextField(blank=True)
     
     def __str__(self):
         return self.username
