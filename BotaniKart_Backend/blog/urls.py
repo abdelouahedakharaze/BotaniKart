e34dc729bfc9c50ccdc00f990bf9a7a927657ea2
abdelouahed akharaze <112736714+abdelouahedakharaze@users.noreply.github.com>
2024-10-12 17:31:55 +0100

    Modify BotaniKart_Backend/blog/urls.py

--- BotaniKart_Backend/blog/urls.py
+++ BotaniKart_Backend/blog/urls.py
@@ -1,10 +1,14 @@
 from django.urls import path
-from .views import ArticleList, ArticleDetail, CommentList, CommentDetail, LikeArticle
+from .views import PostList, PostDetail, CategoryList, CategoryDetail, TagList, TagDetail, CommentList, CommentDetail, UserPostList
 
 urlpatterns = [
-    path('articles/', ArticleList.as_view(), name='article-list'),
-    path('articles/<int:pk>/', ArticleDetail.as_view(), name='article-detail'),
-    path('articles/<int:article_id>/comments/', CommentList.as_view(), name='comment-list'),
+    path('posts/', PostList.as_view(), name='post-list'),
+    path('posts/<int:pk>/', PostDetail.as_view(), name='post-detail'),
+    path('categories/', CategoryList.as_view(), name='category-list'),
+    path('categories/<int:pk>/', CategoryDetail.as_view(), name='category-detail'),
+    path('tags/', TagList.as_view(), name='tag-list'),
+    path('tags/<int:pk>/', TagDetail.as_view(), name='tag-detail'),
+    path('posts/<int:post_id>/comments/', CommentList.as_view(), name='comment-list'),
     path('comments/<int:pk>/', CommentDetail.as_view(), name='comment-detail'),
-    path('articles/<int:article_id>/like/', LikeArticle.as_view(), name='like-article'),
+    path('user/posts/', UserPostList.as_view(), name='user-post-list'),
 ]
