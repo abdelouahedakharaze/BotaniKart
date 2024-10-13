5c177bc917e6d6c7ac39c180bac96dad0e16ec0f
abdelouahed akharaze <112736714+abdelouahedakharaze@users.noreply.github.com>
2024-10-13 08:57:35 +0100

    comment in users/views

--- BotaniKart_Backend/users/views.py
+++ BotaniKart_Backend/users/views.py
@@ -4,11 +4,13 @@ from .models import CustomUser, Address
 from .serializers import UserSerializer, AddressSerializer
 from .forms import VendorPaymentForm
 
+# View for user registration
 class RegisterView(generics.CreateAPIView):
     queryset = CustomUser.objects.all()
     permission_classes = (permissions.AllowAny,)
     serializer_class = UserSerializer
 
+# View for user login
 class LoginView(generics.GenericAPIView):
     serializer_class = UserSerializer
     permission_classes = (permissions.AllowAny,)
@@ -17,6 +19,7 @@ class LoginView(generics.GenericAPIView):
         # Implement login logic here
         pass
 
+# View for updating vendor payment information
 class UpdateVendorPayment(generics.UpdateAPIView):
     permission_classes = [permissions.IsAuthenticated]
     serializer_class = UserSerializer
