dbe94a027f0a092ade42d0d6129f9ea54c20777e
abdelouahed akharaze <112736714+abdelouahedakharaze@users.noreply.github.com>
2024-10-13 08:59:24 +0100

    comment in users/urls

--- BotaniKart_Backend/users/urls.py
+++ BotaniKart_Backend/users/urls.py
@@ -2,11 +2,17 @@ from django.urls import path
 from .views import RegisterView, LoginView, UpdateVendorPayment, AddressListCreateView, AddressDetailView
 from rest_framework_simplejwt.views import TokenRefreshView
 
+# Define URL patterns for the application
 urlpatterns = [
+    # URL pattern for user registration
     path('register/', RegisterView.as_view(), name='register'),
+    
+    # URL pattern for user login
     path('login/', LoginView.as_view(), name='login'),
+    
+    # URL pattern for refreshing JWT tokens
     path('token/refresh/', TokenRefreshView.as_view(), name='token_refresh'),
+    
+    # URL pattern for updating vendor payment information
     path('update-payment-info/', UpdateVendorPayment.as_view(), name='update-payment-info'),
-    path('addresses/', AddressListCreateView.as_view(), name='address-list-create'),
-    path('addresses/<int:pk>/', AddressDetailView.as_view(), name='address-detail'),
 ]
