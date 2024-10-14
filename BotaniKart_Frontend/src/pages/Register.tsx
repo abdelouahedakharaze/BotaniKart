621921cb3e75a53576642506525bf9f30b2e1570
abdelouahed akharaze <112736714+abdelouahedakharaze@users.noreply.github.com>
2024-10-14 08:38:03 +0100

    Register.tsx

--- BotaniKart_Frontend/src/pages/Register.tsx
+++ BotaniKart_Frontend/src/pages/Register.tsx
@@ -1,8 +1,15 @@
-// src/pages/Register.tsx
-import React from 'react';
+import React from 'react'
+import RegisterForm from '../components/auth/RegisterForm'
 
-const Register = () => {
-  return <div>Register Page</div>;
-};
+const Register: React.FC = () => {
+  return (
+    <div className="container mx-auto px-4 flex justify-center items-center min-h-screen">
+      <div className="w-full max-w-md">
+        <h1 className="text-3xl font-bold mb-8 text-center">Register</h1>
+        <RegisterForm />
+      </div>
+    </div>
+  )
+}
 
-export default Register;
+export default Register
