cbeff07829f57e4e7a2220aceefbab5662431625
abdelouahed akharaze <112736714+abdelouahedakharaze@users.noreply.github.com>
2024-10-14 08:35:21 +0100

    Login.tsx

--- BotaniKart_Frontend/src/pages/Login.tsx
+++ BotaniKart_Frontend/src/pages/Login.tsx
@@ -1,8 +1,15 @@
-// src/pages/Login.tsx
-import React from 'react';
+import React from 'react'
+import LoginForm from '../components/auth/LoginForm'
 
-const Login = () => {
-  return <div>Login Page</div>;
-};
+const Login: React.FC = () => {
+  return (
+    <div className="container mx-auto px-4 flex justify-center items-center min-h-screen">
+      <div className="w-full max-w-md">
+        <h1 className="text-3xl font-bold mb-8 text-center">Login</h1>
+        <LoginForm />
+      </div>
+    </div>
+  )
+}
 
-export default Login;
+export default Login
