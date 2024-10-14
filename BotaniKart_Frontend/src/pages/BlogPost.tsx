f90cebdde6d3fbb1fbfaa0731fd5d47fa828d3cf
abdelouahed akharaze <112736714+abdelouahedakharaze@users.noreply.github.com>
2024-10-14 08:37:31 +0100

    BlogPost.tsx

--- BotaniKart_Frontend/src/pages/BlogPost.tsx
+++ BotaniKart_Frontend/src/pages/BlogPost.tsx
@@ -1,12 +1,29 @@
-import React from 'react';
+import React from 'react'
+import { useParams } from 'react-router-dom'
+import PostDetail from '../components/blog/PostDetail'
+import CommentList from '../components/blog/CommentList'
+import CommentForm from '../components/blog/CommentForm'
 
 const BlogPost: React.FC = () => {
-  return (
-    <div>
-      <h1>Blog Post</h1>
-      <p>This is a blog post page.</p>
-    </div>
-  );
-};
+  const { id } = useParams<{ id: string }>()
 
-export default BlogPost;
+  if (!id) {
+    return <div>Post not found</div>
+  }
+
+  return (
+    <div className="container mx-auto px-4">
+      <PostDetail id={id} />
+      <div className="mt-8">
+        <h2 className="text-2xl font-bold mb-4">Comments</h2>
+        <CommentList postId={id} />
+        <div className="mt-4">
+          <h3 className="text-xl font-semibold mb-2">Add a Comment</h3>
+          <CommentForm postId={id} />
+        </div>
+      </div>
+    </div>
+  )
+}
+
+export default BlogPost
