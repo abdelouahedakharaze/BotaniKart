bb172968602ee81526428c5b7097ab98583665e5
abdelouahed akharaze <112736714+abdelouahedakharaze@users.noreply.github.com>
2024-10-14 08:38:49 +0100

    ErrorPage.tsx

--- BotaniKart_Frontend/src/pages/ErrorPage.tsx
+++ BotaniKart_Frontend/src/pages/ErrorPage.tsx
@@ -1,12 +1,17 @@
-import React from 'react';
+import React from 'react'
+import { Link } from 'react-router-dom'
+import { Button } from '../components/common/Button'
 
 const ErrorPage: React.FC = () => {
   return (
-    <div>
-      <h1>Error</h1>
-      <p>Sorry, something went wrong.</p>
+    <div className="flex flex-col items-center justify-center min-h-screen bg-gray-100">
+      <h1 className="text-4xl font-bold mb-4">Oops! Something went wrong.</h1>
+      <p className="text-xl mb-8">We're sorry, but we couldn't find the page you were looking for.</p>
+      <Link to="/">
+        <Button>Go back to homepage</Button>
+      </Link>
     </div>
-  );
-};
+  )
+}
 
-export default ErrorPage;
+export default ErrorPage
