7817ecaa626e02769c35fba9ead5577296a09362
abdelouahed akharaze <112736714+abdelouahedakharaze@users.noreply.github.com>
2024-10-14 08:36:43 +0100

    Orders.tsx

--- BotaniKart_Frontend/src/pages/Orders.tsx
+++ BotaniKart_Frontend/src/pages/Orders.tsx
@@ -1,11 +1,13 @@
-import React from 'react';
+import React from 'react'
+import OrderList from '../components/orders/OrderList'
 
 const Orders: React.FC = () => {
   return (
-    <div>
-      <h1>Orders Page</h1>
+    <div className="container mx-auto px-4">
+      <h1 className="text-3xl font-bold mb-8">Your Orders</h1>
+      <OrderList />
     </div>
-  );
-};
+  )
+}
 
-export default Orders;
+export default Orders
