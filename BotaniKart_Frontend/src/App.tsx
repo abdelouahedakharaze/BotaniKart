c5426162098cc841acd793f6fa392d81f0a3c79b
abdelouahed akharaze <112736714+abdelouahedakharaze@users.noreply.github.com>
2024-10-14 10:02:08 +0100

    commented out the pages that dont work , still testing and fixing them

--- BotaniKart_Frontend/src/App.tsx
+++ BotaniKart_Frontend/src/App.tsx
@@ -1,7 +1,7 @@
 import { BrowserRouter as Router, Route, Routes } from 'react-router-dom'
-import { AuthProvider } from './contexts/AuthContext'
-import { CartProvider } from './contexts/CartContext'
-import { ThemeProvider } from './contexts/ThemeContext'
+// import { AuthProvider } from './contexts/AuthContext'
+// import { CartProvider } from './contexts/CartContext'
+// import { ThemeProvider } from './contexts/ThemeContext'
 import Layout from './components/layout/Layout'
 import Home from './pages/Home'
 import Products from './pages/Products'
@@ -21,30 +21,30 @@ import ErrorPage from './pages/ErrorPage'
 function App() {
   return (
     <Router>
-      <ThemeProvider>
+      {/* <ThemeProvider>
         <AuthProvider>
-          <CartProvider>
+          <CartProvider> */}
             <Layout>
               <Routes>
                 <Route path="/" element={<Home />} />
-                <Route path="/products" element={<Products />} />
-                <Route path="/product/:id" element={<ProductDetail />} />
+                {/* <Route path="/products" element={<Products />} /> */}
+                {/* <Route path="/product/:id" element={<ProductDetail />} /> */}
                 <Route path="/blog" element={<Blog />} />
-                <Route path="/blog/:id" element={<BlogPost />} />
-                <Route path="/cart" element={<Cart />} />
-                <Route path="/checkout" element={<Checkout />} />
-                <Route path="/login" element={<Login />} />
-                <Route path="/register" element={<Register />} />
-                <Route path="/profile" element={<Profile />} />
+                {/* <Route path="/blog/:id" element={<BlogPost />} /> */}
+                {/* <Route path="/cart" element={<Cart />} /> */}
+                {/* <Route path="/checkout" element={<Checkout />} /> */}
+                {/* <Route path="/login" element={<Login />} /> */}
+                {/* <Route path="/register" element={<Register />} /> */}
+                {/* <Route path="/profile" element={<Profile />} /> */}
                 <Route path="/orders" element={<Orders />} />
-                <Route path="/wishlist" element={<Wishlist />} />
-                <Route path="/loyalty" element={<LoyaltyProgram />} />
+                {/* <Route path="/wishlist" element={<Wishlist />} /> */}
+                {/* <Route path="/loyalty" element={<LoyaltyProgram />} /> */}
                 <Route path="*" element={<ErrorPage />} />
               </Routes>
             </Layout>
-          </CartProvider>
+          {/* </CartProvider>
         </AuthProvider>
-      </ThemeProvider>
+      </ThemeProvider> */}
     </Router>
   )
 }
