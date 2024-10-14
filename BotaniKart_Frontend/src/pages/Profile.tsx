aa1c1328ef32725a2edf1d8ae4898fa323baa3c8
abdelouahed akharaze <112736714+abdelouahedakharaze@users.noreply.github.com>
2024-10-14 08:37:40 +0100

    Profile.tsx

--- BotaniKart_Frontend/src/pages/Profile.tsx
+++ BotaniKart_Frontend/src/pages/Profile.tsx
@@ -1,11 +1,20 @@
-import React from 'react';
+import React from 'react'
+import { useAuth } from '../hooks/useAuth'
+import ProfileForm from '../components/user/ProfileForm'
 
 const Profile: React.FC = () => {
-  return (
-    <div>
-      <h1>Profile Page</h1>
-    </div>
-  );
-};
+  const { user } = useAuth()
 
-export default Profile;
+  if (!user) {
+    return <div>Please log in to view your profile.</div>
+  }
+
+  return (
+    <div className="container mx-auto px-4">
+      <h1 className="text-3xl font-bold mb-8">Your Profile</h1>
+      <ProfileForm initialData={user} onSubmit={console.log} />
+    </div>
+  )
+}
+
+export default Profile
