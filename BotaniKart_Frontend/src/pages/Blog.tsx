181a5e25fc23bd16de3982fc927ab3addd7fbe50
abdelouahed akharaze <112736714+abdelouahedakharaze@users.noreply.github.com>
2024-10-14 08:35:35 +0100

    Blog.tsx

--- BotaniKart_Frontend/src/pages/Blog.tsx
+++ BotaniKart_Frontend/src/pages/Blog.tsx
@@ -1,12 +1,21 @@
-import React from 'react';
+import React from 'react'
+import PostList from '../components/blog/PostList'
+import CategoryList from '../components/blog/CategoryList'
 
 const Blog: React.FC = () => {
   return (
-    <div>
-      <h1>Blog Page</h1>
-      <p>Welcome to the Blog page!</p>
+    <div className="container mx-auto px-4">
+      <h1 className="text-3xl font-bold mb-8">Blog</h1>
+      <div className="grid grid-cols-1 md:grid-cols-4 gap-8">
+        <div className="md:col-span-1">
+          <CategoryList />
+        </div>
+        <div className="md:col-span-3">
+          <PostList />
+        </div>
+      </div>
     </div>
-  );
-};
+  )
+}
 
-export default Blog;
+export default Blog
