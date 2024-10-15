6ab109589ac6f1fd9de4f0f880a1a3024e6802d3
abdelouahed akharaze <112736714+abdelouahedakharaze@users.noreply.github.com>
2024-10-15 19:54:49 +0100

    admin.py

--- BotaniKart_Backend/products/admin.py
+++ BotaniKart_Backend/products/admin.py
@@ -3,7 +3,7 @@ from django.contrib import admin
 # Register your models here.
 from  .models import (Product, Category, SubCategory,
                       ProductAttribute, Coupon,ProductSKU,
-                      Purchase,coupon,Wishlist)
+                      Purchase,Coupon,Wishlist)
 
 
 
@@ -11,9 +11,9 @@ admin.site.register(Product)
 admin.site.register(Category)
 admin.site.register(SubCategory)
 admin.site.register(ProductAttribute)
-admin.site.register(Coupon)
+# admin.site.register(Coupon)
 admin.site.register(ProductSKU)
 admin.site.register(Purchase)
-admin.site.register(coupon)
+admin.site.register(Coupon)
 admin.site.register(Wishlist)
 # This code registers the Product, Category, SubCategory, ProductAttribute, Coupon, ProductSKU, Purchase, coupon, and Wishlist models with the Django admin site.
