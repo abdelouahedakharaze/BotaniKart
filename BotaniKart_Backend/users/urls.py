76a085aa0a17d6f210a8b0fb017bf0cb830f9a9d
abdelouahed akharaze <112736714+abdelouahedakharaze@users.noreply.github.com>
2024-10-20 12:35:49 +0100

    modified some of the ui elements

--- BotaniKart_Backend/users/urls.py
+++ BotaniKart_Backend/users/urls.py
@@ -1,18 +1,34 @@
 from django.urls import path
-from .views import RegisterView, LoginView, AddressListCreateView, AddressDetailView
+from .views import (
+    AddressManageView,
+    RegisterView,
+    LoginView,
+    UserProfileUpdateView,
+    UserProfileView,
+    VendorRegistrationView,
+    
+)
 from rest_framework_simplejwt.views import TokenRefreshView
 
 # Define URL patterns for the application
 urlpatterns = [
     # URL pattern for user registration
     path('register/', RegisterView.as_view(), name='register'),
-    
+
     # URL pattern for user login
     path('login/', LoginView.as_view(), name='login'),
-    
+
     # URL pattern for refreshing JWT tokens
     path('token/refresh/', TokenRefreshView.as_view(), name='token_refresh'),
-    
-    # # URL pattern for updating vendor payment information
-    # path('update-payment-info/', UpdateVendorPayment.as_view(), name='update-payment-info'),
+
+    # URL pattern for updating user profile information
+    path('profile/update/', UserProfileUpdateView.as_view(), name='profile_update'),
+
+    path('profile/', UserProfileView.as_view(), name='user-profile'),
+
+    # URL pattern for vendor registration
+    path('vendor/register/', VendorRegistrationView.as_view(), name='vendor_register'),
+
+    # URL pattern for managing a single address
+    path('address/', AddressManageView.as_view(), name='address_detail'),
 ]
