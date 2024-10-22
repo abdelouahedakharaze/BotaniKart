08c5bf530e18552dc3ddd348033b4b041e8f4a15
abdelouahed akharaze <112736714+abdelouahedakharaze@users.noreply.github.com>
2024-10-22 11:56:23 +0100

    removed one line

--- BotaniKart_Backend/BotaniKart_Backend/urls.py
+++ BotaniKart_Backend/BotaniKart_Backend/urls.py
@@ -5,7 +5,6 @@ from django.conf.urls.static import static
 from rest_framework import permissions
 from drf_yasg.views import get_schema_view
 from drf_yasg import openapi
-from utils.translation import LanguageView
 
 # Schema view for Swagger
 schema_view = get_schema_view(
