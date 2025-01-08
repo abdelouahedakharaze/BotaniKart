5d0bcd64502ff9f9a96541d4ddac4b0b4db0d3f8
abdelouahed akharaze <112736714+abdelouahedakharaze@users.noreply.github.com>
2025-01-08 20:04:57 +0100

    settings and general urls

--- Botanikart_backend/BotaniKart_Backend/urls.py
+++ Botanikart_backend/BotaniKart_Backend/urls.py
@@ -1,22 +1,22 @@
-"""
-URL configuration for BotaniKart_Backend project.
-
-The `urlpatterns` list routes URLs to views. For more information please see:
-    https://docs.djangoproject.com/en/5.1/topics/http/urls/
-Examples:
-Function views
-    1. Add an import:  from my_app import views
-    2. Add a URL to urlpatterns:  path('', views.home, name='home')
-Class-based views
-    1. Add an import:  from other_app.views import Home
-    2. Add a URL to urlpatterns:  path('', Home.as_view(), name='home')
-Including another URLconf
-    1. Import the include() function: from django.urls import include, path
-    2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
-"""
 from django.contrib import admin
-from django.urls import path
+from django.urls import path, include
 
+from django.conf import settings
+from django.conf.urls.static import static
+from django.views.generic import TemplateView
+
+# Main backend URL Configuration
 urlpatterns = [
-    path('admin/', admin.site.urls),
+    path('admin/', admin.site.urls),  # Admin panel access
+    
+    # API endpoints for different apps
+    path('api/products/', include('base.urls.product_urls')),  # Product-related endpoints
+    path('api/users/', include('base.urls.user_urls')),  # User-related endpoints
+    path('api/orders/', include('base.urls.order_urls')),  # Order-related endpoints
 ]
+
+# Serving media files during development
+urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+
+# Serving static files
+urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
