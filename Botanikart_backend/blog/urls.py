f12fee3cb59d596bb29b62cc9cdec1f44307a66f
abdelouahed akharaze <112736714+abdelouahedakharaze@users.noreply.github.com>
2025-01-26 21:51:12 +0100

    updating blog urls

--- Botanikart_backend/blog/urls.py
+++ Botanikart_backend/blog/urls.py
@@ -1,11 +1,14 @@
-# blog/urls.py
-from django.urls import path, include
-from rest_framework.routers import DefaultRouter
-from .views import PostViewSet
-
-router = DefaultRouter()
-router.register(r'posts', PostViewSet)
+from django.urls import path
+from .views import PostViewSet, CommentViewSet
 
 urlpatterns = [
-    path('', include(router.urls)),
+    # Post URLs
+    path('posts/', PostViewSet.as_view({'get': 'list', 'post': 'create'}), name='post-list'),
+    path('posts/<int:pk>/', PostViewSet.as_view({'get': 'retrieve', 'put': 'update', 'delete': 'destroy'}), name='post-detail'),
+    path('posts/<int:pk>/add_comment/', PostViewSet.as_view({'post': 'add_comment'}), name='post-add-comment'),
+    path('posts/<int:pk>/toggle_heart/', PostViewSet.as_view({'post': 'add_heart'}), name='post-toggle-heart'),
+
+    # Comment URLs
+    path('comments/', CommentViewSet.as_view({'get': 'list', 'post': 'create'}), name='comment-list'),
+    path('comments/<int:pk>/', CommentViewSet.as_view({'get': 'retrieve', 'put': 'update', 'delete': 'destroy'}), name='comment-detail'),
 ]
