a90aee7eaac8df620f08f6f253b97967b7fb8aeb
abdelouahed akharaze <112736714+abdelouahedakharaze@users.noreply.github.com>
2025-01-27 00:19:39 +0100

    allow to see the comment username

--- Botanikart_backend/blog/serializers.py
+++ Botanikart_backend/blog/serializers.py
@@ -1,11 +1,19 @@
 from rest_framework import serializers
 from .models import Post, Comment, Heart
+from django.contrib.auth.models import User
+
+
+class UserSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = User  
+        fields = ['id', 'username']
 
 class CommentSerializer(serializers.ModelSerializer):
+    user = UserSerializer(read_only=True)  # Add this line
+    
     class Meta:
         model = Comment
         fields = ['id', 'text', 'created_at', 'user']
-        read_only_fields = ['user'] 
 
 
 class PostSerializer(serializers.ModelSerializer):
