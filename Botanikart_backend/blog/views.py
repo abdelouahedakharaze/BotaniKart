8cd4c424796998a2990ad9225392d617ae4546c1
abdelouahed akharaze <112736714+abdelouahedakharaze@users.noreply.github.com>
2025-01-26 21:51:30 +0100

    updating blog views

--- Botanikart_backend/blog/views.py
+++ Botanikart_backend/blog/views.py
@@ -1,31 +1,53 @@
-# blog/views.py
 from rest_framework import status, viewsets
 from rest_framework.decorators import action
 from rest_framework.response import Response
 from .models import Post, Comment, Heart
 from .serializers import PostSerializer, CommentSerializer, HeartSerializer
-from rest_framework.permissions import IsAuthenticated, IsAuthenticatedOrReadOnly
 
 class PostViewSet(viewsets.ModelViewSet):
     queryset = Post.objects.all()
     serializer_class = PostSerializer
-    permission_classes = [IsAuthenticatedOrReadOnly]  # Allow read-only for everyone, modify only for authenticated users
 
-    def perform_create(self, serializer):
-        # Automatically assign the logged-in user as the author of the post
-        serializer.save(author=self.request.user)
+    def list(self, request):
+        posts = self.get_queryset()
+        serializer = self.get_serializer(posts, many=True)
+        return Response(serializer.data)
 
-    # views.py
-@action(detail=True, methods=['post'], permission_classes=[IsAuthenticated])
-def add_comment(self, request, pk=None):
-    post = self.get_object()
-    serializer = CommentSerializer(data=request.data)
-    if serializer.is_valid():
-        serializer.save(post=post, user=request.user)
-        return Response(serializer.data, status=status.HTTP_201_CREATED)
-    return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+    def create(self, request):
+        serializer = self.get_serializer(data=request.data)
+        if serializer.is_valid():
+            serializer.save()
+            return Response(serializer.data, status=status.HTTP_201_CREATED)
+        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
 
-    @action(detail=True, methods=['post'], permission_classes=[IsAuthenticated])
+    def retrieve(self, request, pk=None):
+        post = self.get_object()
+        serializer = self.get_serializer(post)
+        return Response(serializer.data)
+
+    def update(self, request, pk=None):
+        post = self.get_object()
+        serializer = self.get_serializer(post, data=request.data)
+        if serializer.is_valid():
+            serializer.save()
+            return Response(serializer.data)
+        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+
+    def destroy(self, request, pk=None):
+        post = self.get_object()
+        post.delete()
+        return Response(status=status.HTTP_204_NO_CONTENT)
+
+    @action(detail=True, methods=['post'])
+    def add_comment(self, request, pk=None):
+        post = self.get_object()
+        serializer = CommentSerializer(data=request.data)
+        if serializer.is_valid():
+            serializer.save(post=post, user=request.user)
+            return Response(serializer.data, status=status.HTTP_201_CREATED)
+        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+
+    @action(detail=True, methods=['post'])
     def add_heart(self, request, pk=None):
         post = self.get_object()
         user = request.user
@@ -33,20 +55,39 @@ def add_comment(self, request, pk=None):
         if created:
             return Response({'status': 'liked'}, status=status.HTTP_201_CREATED)
         else:
-            return Response({'status': 'already liked'}, status=status.HTTP_400_BAD_REQUEST)
-
-    def get_permissions(self):
-        if self.action in ['create', 'update', 'destroy']:
-            self.permission_classes = [IsAuthenticated]
-        elif self.action == 'add_comment':
-            self.permission_classes = [IsAuthenticated]
-        return super().get_permissions()
-
+            heart.delete()
+            return Response({'status': 'unliked'}, status=status.HTTP_200_OK)
 
 class CommentViewSet(viewsets.ModelViewSet):
     queryset = Comment.objects.all()
     serializer_class = CommentSerializer
-    permission_classes = [IsAuthenticatedOrReadOnly]  # Only authenticated users can create comments
 
-    def perform_create(self, serializer):
-        serializer.save(user=self.request.user)
+    def list(self, request):
+        comments = self.get_queryset()
+        serializer = self.get_serializer(comments, many=True)
+        return Response(serializer.data)
+
+    def create(self, request):
+        serializer = self.get_serializer(data=request.data)
+        if serializer.is_valid():
+            serializer.save(user=request.user)
+            return Response(serializer.data, status=status.HTTP_201_CREATED)
+        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+
+    def retrieve(self, request, pk=None):
+        comment = self.get_object()
+        serializer = self.get_serializer(comment)
+        return Response(serializer.data)
+
+    def update(self, request, pk=None):
+        comment = self.get_object()
+        serializer = self.get_serializer(comment, data=request.data)
+        if serializer.is_valid():
+            serializer.save()
+            return Response(serializer.data)
+        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+
+    def destroy(self, request, pk=None):
+        comment = self.get_object()
+        comment.delete()
+        return Response(status=status.HTTP_204_NO_CONTENT)
